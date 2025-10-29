cc423377d4260cbe3b7c88eddee9b970ae80df3d
Felipe <127034748+Felipe-student@users.noreply.github.com>
2025-10-29 11:19:19 -0300

    Atualização e corração de route_tables e entradas security_groups

--- modules/conectividade/main.tf
+++ modules/conectividade/main.tf
@@ -11,16 +11,19 @@ resource "aws_subnet" "dmz" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.0.0/26"
     map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "app" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.0.64/26"
+    availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "bd" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.0.128/26"
+    availability_zone = "us-east-1c"
 }
 
 resource "aws_eip" "nat" {
@@ -32,3 +35,63 @@ resource "aws_nat_gateway" "nat_gateway" {
     subnet_id = aws_subnet.dmz.id
 }
 
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "dmz_rota" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dmz_rota.id
+  }
+  tags = {
+    Name = "dmz-rota"
+  }
+}
+
+resource "aws_route_table_association" "dmz_assoc" {
+  subnet_id = aws_subnet.dmz.id
+  route_table_id = aws_route_table.dmz_rota
+}
+
+resource "aws_route_table" "app_rota" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "app-rota"
+  }
+}
+
+resource "aws_route_table_association" "app_assoc" {
+  subnet_id = aws_subnet.app.id
+  route_table_id = aws_route_table.app_rota
+}
+
+resource "aws_route_table" "bd_rota" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "bd-rota"
+  }
+}
+
+resource "aws_route_table_association" "bd_assoc" {
+  subnet_id = aws_subnet.bd.id
+  route_table_id = aws_route_table.bd_rota.id
+}
